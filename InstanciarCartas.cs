1e5a88834a43dbbeb0cfd242aaed32c4427aa317
fandurc <fabianurrutiacruz@gmail.com>
2024-08-12 12:54:56 -0400

    Update InstanciarCartas.cs

--- InstanciarCartas.cs
+++ InstanciarCartas.cs
@@ -2,36 +2,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
-
 public class InstanciarCartas : MonoBehaviour
 {
-    public GameObject cartaPrefab; // Prefab de la carta
-    public Transform gridTransform; // Transform del Grid Layout Group
-    public List<Image> cartasEnHierarchy; // Lista de imágenes de cartas en el hierarchy
-    public Color selectedColor = Color.green; // Color al que cambiará al seleccionarse
-    public Color originalColor = Color.white; // Color original de la carta
+    public GameObject cartaPrefab;
+    public Transform gridTransform;
+    public List<Image> cartasEnHierarchy;
+    public Color selectedColor = Color.green;
+    public Color originalColor = Color.white;
+
+    private List<int> espacioCartas = new List<int>();
 
     void Start()
     {
-        foreach (var carta in cartasEnHierarchy)
+        for (int i = 0; i < cartasEnHierarchy.Count; i++)
         {
             GameObject nuevaCarta = Instantiate(cartaPrefab, gridTransform);
             Image cartaImage = nuevaCarta.GetComponent<Image>();
-            cartaImage.sprite = carta.sprite;
+            cartaImage.sprite = cartasEnHierarchy[i].sprite;
 
-            // Configura otras propiedades de la carta usando la clase Carta
             Carta scriptCarta = nuevaCarta.GetComponent<Carta>();
-            scriptCarta.numero = carta.GetComponent<Carta>().numero;
-            scriptCarta.tipoCarta = carta.GetComponent<Carta>().tipoCarta;
-            scriptCarta.rareza = carta.GetComponent<Carta>().rareza;
-            scriptCarta.unica = carta.GetComponent<Carta>().unica; // Inicializar el nuevo campo
-            scriptCarta.contadorText = nuevaCarta.GetComponentInChildren<TextMeshProUGUI>(); // Referencia al componente de texto
-            // Configura cualquier otra propiedad que tengas en la clase Carta
+            scriptCarta.numero = cartasEnHierarchy[i].GetComponent<Carta>().numero;
+            scriptCarta.tipoCarta = cartasEnHierarchy[i].GetComponent<Carta>().tipoCarta;
+            scriptCarta.rareza = cartasEnHierarchy[i].GetComponent<Carta>().rareza;
+            scriptCarta.unica = cartasEnHierarchy[i].GetComponent<Carta>().unica;
+            scriptCarta.contadorText = nuevaCarta.GetComponentInChildren<TextMeshProUGUI>();
+
+            // Guardar el número de espacio correlativo
+            espacioCartas.Add(i + 1);
 
             // Agregar el componente SelectableItem
             SelectableItem selectableItem = nuevaCarta.AddComponent<SelectableItem>();
             selectableItem.selectedColor = selectedColor;
             selectableItem.originalColor = originalColor;
+
+            // Añadir evento para mostrar número de espacio y número de carta
+            nuevaCarta.AddComponent<MostrarNumeroCarta>().Configurar(i + 1, scriptCarta.numero);
         }
     }
 }
